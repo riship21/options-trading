71f1e53003b15160ff3e83e3f8c65e3f54906737
krempy21 <rishi2109@vt.edu>
2023-08-23 14:03:10 -0400

    rounded the percent profit

--- basic_options_calc.py
+++ basic_options_calc.py
@@ -1,15 +1,15 @@
 import decimal
 
-buying_power = decimal.Decimal (input("Enter your buying power: "))
-amount = decimal.Decimal (input("Enter the money you put in: "))
+buying_power = decimal.Decimal(input("Enter your buying power: "))
+amount = decimal.Decimal(input("Enter the money you put in: "))
 
 if amount > buying_power:
     print("You don't have enough buying power")
     exit()
 
 
-entry_point = decimal.Decimal (input("Enter the premium price you entered: "))
-contracts_bought = int ( (amount / entry_point) / 100 )
+entry_point = decimal.Decimal(input("Enter the premium price you entered: "))
+contracts_bought = int((amount / entry_point) / 100)
 print(f"You bought {contracts_bought} contracts")
 
 
@@ -17,11 +17,12 @@ exit_point = decimal.Decimal(input("Enter the price you sold: "))
 
 
 buying_power_left = buying_power - amount
-profit =  decimal.Decimal ((exit_point - entry_point) * contracts_bought * 100)
-percent_profit = decimal.Decimal ((profit / amount) * 100)
+profit = decimal.Decimal((exit_point - entry_point) * contracts_bought * 100)
+percent_profit = round(((profit / amount) * 100), 2)
 
 print(f"You have ${buying_power_left} left in your buying power")
 print()
-print(f"Your profit is ${profit} and your percent profit is {percent_profit}% \n")
+print(
+    f"Your profit is ${profit} and your percent profit is {percent_profit}% \n")
 print("no more spy 0dte for me")
 print()
