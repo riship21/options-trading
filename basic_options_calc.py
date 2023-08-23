a9770bfe2ed0a24451cb22f9670e4f1bf7020342
Rishi Patel <rishi2109@vt.edu>
2023-08-23 13:31:46 -0400

    Add files via upload

---
+++ basic_options_calc.py
@@ -0,0 +1,25 @@
+import decimal
+
+buying_power = decimal.Decimal (input("Enter your buying power: "))
+amount = decimal.Decimal (input("Enter the money you put in: "))
+
+if amount > buying_power:
+    print("You don't have enough buying power")
+    exit()
+
+
+entry_point = decimal.Decimal (input("Enter the premium price you entered: "))
+contracts_bought = int ( (amount / entry_point) / 100 )
+print(f"You bought {contracts_bought} contracts")
+
+
+exit_point = decimal.Decimal(input("Enter the price you sold: "))
+
+
+buying_power_left = buying_power - amount
+profit =  decimal.Decimal ((exit_point - entry_point) * contracts_bought * 100)
+percent_profit = decimal.Decimal ((profit / amount) * 100)
+
+print(f"You have ${buying_power_left} left in your buying power")
+print()
+print(f"Your profit is ${profit} and your percent profit is {percent_profit}")
